5a451e1cbbdee0b9367d96fd7918899e213d7486
ptu1710 <20522100@gm.uit.edu.vn>
2023-01-09 13:58:31 +0700

    Routes and Controllers

---
+++ src/app/controllers/SiteControllers.js
@@ -0,0 +1,14 @@
+class SiteControllers {
+
+    // [GET] /
+    home(req, res) {
+        res.render('home')
+    }
+
+    // [GET] /search
+    search(req, res) {
+        res.render('search')
+    }
+}
+
+module.exports = new SiteControllers
--- src/index.js
+++ src/index.js
@@ -5,10 +5,17 @@ const handlebars = require('express-handlebars')
 const app = express()
 const port = 3000
 
+const route = require('./routes')
+
 app.use(express.static('./src/public'))
 
+app.use(express.urlencoded({
+  extended: true
+}))
+app.use(express.json())
+
 // HTTP logger
-app.use(morgan('short'))
+// app.use(morgan('short'))
 
 // Template engine
 app.engine('hbs', handlebars.engine({
@@ -17,14 +24,10 @@ app.engine('hbs', handlebars.engine({
 app.set('view engine', 'hbs')
 app.set('views', './src/resources/views')
 
-app.get('/', (req, res) => {
-    res.render('home')
-})
+// Routes Init
+route(app)
 
-app.get('/news', (req, res) => {
-  res.render('news')
-})
-
+// Listening
 app.listen(port, () => {
   console.log(`Example app listening on port ${port}`)
 })
