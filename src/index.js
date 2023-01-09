bd5b6441382696fb887a4f34ca52adce7142bbce
ptu1710 <20522100@gm.uit.edu.vn>
2023-01-09 14:54:35 +0700

    Install Prettier Lint-Staged Husky

--- src/index.js
+++ src/index.js
@@ -1,19 +1,19 @@
-const express = require('express');
-const morgan = require('morgan');
-const handlebars = require('express-handlebars');
+const express = require("express");
+const morgan = require("morgan");
+const handlebars = require("express-handlebars");
 
 const app = express();
 const port = 3000;
 
-const route = require('./routes');
+const route = require("./routes");
 
-app.use(express.static('./src/public'));
+app.use(express.static("./src/public"));
 
 app.use(
-                express.urlencoded({
-                    extended: true,
-                }),
-            );
+  express.urlencoded({
+    extended: true,
+  })
+);
 app.use(express.json());
 
 // HTTP logger
@@ -21,18 +21,18 @@ app.use(express.json());
 
 // Template engine
 app.engine(
-    'hbs',
-    handlebars.engine({
-        extname: '.hbs',
-    }),
+  "hbs",
+  handlebars.engine({
+    extname: ".hbs",
+  })
 );
-app.set('view engine', 'hbs');
-app.set('views', './src/resources/views');
+app.set("view engine", "hbs");
+app.set("views", "./src/resources/views");
 
 // Routes Init
 route(app);
 
 // Listening
 app.listen(port, () => {
-    console.log(`Example app listening on port ${port}`);
+  console.log(`Example app listening on port ${port}`);
 });
